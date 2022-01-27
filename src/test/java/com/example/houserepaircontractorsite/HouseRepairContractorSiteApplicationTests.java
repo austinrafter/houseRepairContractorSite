e347a87966033816e0784bb6c638d573a92a7dc4
Austin Rafter <austin.rafter@sjsu.edu>
2022-01-27 12:33:14 -0800

    Initial commit

---
+++ src/test/java/com/example/houserepaircontractorsite/HouseRepairContractorSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.houserepaircontractorsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseRepairContractorSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
